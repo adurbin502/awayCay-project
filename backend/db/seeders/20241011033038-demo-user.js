f579dfe084b2a9c549099f90fc427b8b7ae5cec2
Andrew <a.durbin502@gmail.com>
2024-10-14 18:36:53 -0400

    debugged for re-deployment

--- backend/db/seeders/20241011033038-demo-user.js
+++ backend/db/seeders/20241011033038-demo-user.js
@@ -3,14 +3,15 @@
 const { User } = require("../models");
 const bcrypt = require("bcryptjs");
 
-let options = {};
+let options = { tableName: "Users" };
 if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA; // define your schema in options object
+  options.schema = process.env.SCHEMA; // Define schema for production
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await User.bulkCreate(
+    await queryInterface.bulkInsert(
+      options,
       [
         {
           email: "demo@user.io",
@@ -33,18 +34,11 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users";
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(
       options,
       {
-        username: {
-          [Op.in]: [
-            "Demo-lition",
-            "FakeUser1",
-            "FakeUser2",
-          ],
-        },
+        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
       },
       {}
     );

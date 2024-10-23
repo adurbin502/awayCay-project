c12f0ff033ce6b43a56a13e83e93b79606091b41
Andrew <a.durbin502@gmail.com>
2024-10-23 19:59:31 -0400

    syntax revision for spot-images

--- backend/db/models/spot-images.js
+++ backend/db/models/spot-images.js
@@ -1,34 +1,37 @@
 'use strict';
 const { Model } = require('sequelize');
 
-class SpotImage extends Model {
-  static associate(models) {
-    SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' });
-  }
-}
-
-SpotImage.init({
-  spotId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Spots',
-      key: 'id'
+module.exports = (sequelize, DataTypes) => {
+  class SpotImage extends Model {
+    static associate(models) {
+      // Define association here
+      SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' });
     }
-  },
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  preview: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false
   }
-}, {
-  sequelize,
-  modelName: 'SpotImage',
-});
 
-module.exports = SpotImage;
+  SpotImage.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Spots',
+        key: 'id'
+      }
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    preview: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  }, {
+    sequelize,
+    modelName: 'SpotImage',
+  });
+
+  return SpotImage;
+};
 

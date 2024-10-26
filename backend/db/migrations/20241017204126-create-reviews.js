56797bf695fba92fbe6f8a5686df2e6510ec6296
Andrew <a.durbin502@gmail.com>
2024-10-26 18:10:55 -0400

    migrations and syntax fix

--- backend/db/migrations/20241017204126-create-reviews.js
+++ backend/db/migrations/20241017204126-create-reviews.js
@@ -48,7 +48,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages', options);
     options.tableName = 'Reviews';
     return queryInterface.dropTable(options);
   }

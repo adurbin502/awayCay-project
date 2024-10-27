b82b32d2e4f866ecead3cf7f05444dc7bc6dd4c2
Andrew <a.durbin502@gmail.com>
2024-10-27 16:13:43 -0400

    debug for api fixes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -44,23 +44,6 @@ const validateSignup = [
 router.post("/", validateSignup, async (req, res) => {
   const { email, password, username, firstName, lastName } =
     req.body;
-
-    const existingUser = await User.findOne({
-      where: {
-        [Op.or]: [{ email }, { username }]
-      }
-    });
-
-    if (existingUser) {
-      const errors = {};
-      if (existingUser.email === email) errors.email = "User with that email already exists";
-      if (existingUser.username === username) errors.username = "User with that username already exists";
-
-      const err = new Error("User already exists");
-      err.status = 500;
-      err.errors = errors;
-      return next(err);
-    }
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({
     email,
